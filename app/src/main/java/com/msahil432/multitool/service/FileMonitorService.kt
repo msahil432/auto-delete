4d836970f667686954f8948b838b6152e19178d3
Sahil Malik <sahil.malik@comprotechnologies.com>
2026-08-16 04:02:12 +0530

    bug: app start crash

--- app/src/main/java/com/msahil432/multitool/service/FileMonitorService.kt
+++ app/src/main/java/com/msahil432/multitool/service/FileMonitorService.kt
@@ -45,6 +45,7 @@ class FileMonitorService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
             startForeground(1, createNotification(), ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE)
         } else {
