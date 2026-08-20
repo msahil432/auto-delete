90de022d9b74d5aaf12272a6a07d9bcf975da957
Sahil Malik <sahil.malik@comprotechnologies.com>
2026-08-20 19:40:26 +0530

    tests: fix

--- app/src/test/java/com/msahil432/multitool/tracking/ScreenUnlockReceiverTest.kt
+++ app/src/test/java/com/msahil432/multitool/tracking/ScreenUnlockReceiverTest.kt
@@ -22,7 +22,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [35])
 class ScreenUnlockReceiverTest {
 
     private lateinit var db: AppDatabase
